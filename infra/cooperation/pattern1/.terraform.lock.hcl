5c012dc15848066b35a4025450bdb0c63b05492e
yuto.eguma <yuto.eguma@gmail.com>
2021-08-24 00:01:40 +0900

    pattern1 の user を実装

---
+++ infra/cooperation/pattern1/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version = "3.55.0"
+  hashes = [
+    "h1:Ls8MD4Olzybw9n0mP5Lr1S2PnZzlSKrpxvYN9u2p/dM=",
+  ]
+}
---
+++ infra/cooperation/pattern1/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = var.default_region
+}
+
+module "naming" {
+  source = "../../_module/naming"
+  role   = local.role
+}
+
+resource "aws_iam_user" "user" {
+  name = module.naming.name
+  tags = {
+    name = module.naming.name
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "policy" {
+  user       = aws_iam_user.user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
